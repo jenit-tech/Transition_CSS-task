274e69116cd8bf8f332dc70139bd20adc2a90442
jenit-tech <jenitjosephjose@gmail.com>
2024-12-02 08:04:43 +0530

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './index.css'
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div className='app-container'>
+                <div className='card-one'>
+                    <img src='riskhawk-logo.png' alt='' className='image-one'/>
+                    <div className='card-two'>
+                        <div className='card-three'>
+
+                        </div>
+
+                    </div>
+
+                </div>
+               
+
+            </div>
+        )
+    }
+}
+
+export default Home
